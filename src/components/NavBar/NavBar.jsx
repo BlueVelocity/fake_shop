bbca1fb4a41cc55ed30d5d9165f343cdb2162a86
BlueVelocity <josephrodney@hotmail.com>
2025-03-16 12:12:31 -0400

    Tests for navbar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,8 +13,9 @@ export default function NavBar({ cartContents }) {
   return (
     <header className={styles.header}>
       <Link to={"/home"} className={styles.shopNameLink}><h1 className={styles.shopName}>Fake Shop</h1></Link>
-      <NavButton icon={homeIcon} alt="Home button" link="/home" />
-      <NavButton icon={bagIcon} alt="Shop button" link="/shop" /> <NavButton icon={shoppingCartIcon} alt="Checkout button" link="/cart" subScript={calcCartQty()} />
+      <NavButton icon={homeIcon} alt="Home button" link="/" />
+      <NavButton icon={bagIcon} alt="Shop button" link="/shop" />
+      <NavButton icon={shoppingCartIcon} alt="Cart button" link="/cart" subScript={calcCartQty()} />
     </header>
   );
 }
--- src/components/NavBar/NavBar.test.jsx
+++ src/components/NavBar/NavBar.test.jsx
@@ -1,16 +1,43 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import NavBar from "./NavBar";
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("NavBar component", () => {
-  it("Renders correct heading on load", () => {
+  beforeEach(() => {
     render(
       <BrowserRouter>
         <NavBar />
       </BrowserRouter>
     );
+  })
+
+  it("Contains store name", () => {
     expect(screen.getByRole("heading").textContent).toMatch("Fake Shop");
   });
+
+  it("Should render a home button that links to '/' route", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: "Home button" });
+    await user.click(button);
+
+    expect(window.location.pathname).toBe("/");
+  })
+
+  it("Should render a shop button that links to '/shop' route", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: "Shop button" });
+    await user.click(button);
+
+    expect(window.location.pathname).toBe("/shop");
+  })
+
+  it("Should render a cart button that links to '/cart' route", async () => {
+    const user = userEvent.setup();
+    const button = screen.getByRole("button", { name: "Cart button" });
+    await user.click(button);
+
+    expect(window.location.pathname).toBe("/cart");
+  })
 });
