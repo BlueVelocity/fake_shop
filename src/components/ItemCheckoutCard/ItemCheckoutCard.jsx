0820e55f55ca4c8ab6a264bd5de03af88149dd15
BlueVelocity <josephrodney@hotmail.com>
2025-03-15 23:00:42 -0400

    Update cart styling

--- src/components/ItemCheckoutCard/ItemCheckoutCard.jsx
+++ src/components/ItemCheckoutCard/ItemCheckoutCard.jsx
@@ -4,8 +4,8 @@ export default function ItemCheckoutCard({ imgUrl, name, price, qty }) {
   return (
     <li className={styles.card}>
       <img src={imgUrl} alt={name} />
-      <span>{name}</span>
-      <span className={styles.price}>${price} x {qty}</span>
+      <span className={styles.itemName}>{name}</span>
+      <span className={styles.price}><span>${price}</span> <span>x {qty}</span><button> - </button></span>
       <span className={styles.price}>${(price * qty).toFixed(2)}</span>
     </li>
   )
